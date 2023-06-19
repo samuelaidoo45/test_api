a4847d5c62304cf52d5fe96c5f43311083710745
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-06-19 08:01:55 -0700

    update

--- app/GraphQL/Mutations/StoreMedicalRecord.php
+++ app/GraphQL/Mutations/StoreMedicalRecord.php
@@ -2,6 +2,13 @@
 
 namespace App\GraphQL\Mutations;
 
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\MedicalRecord;
+use App\Models\MedicalRecordXrayOption;
+use App\Models\MedicalRecordUltrasoundOption;
+
+
 final class StoreMedicalRecord
 {
     /**
@@ -10,15 +17,80 @@ final class StoreMedicalRecord
      */
     public function __invoke($_, array $args)
     {
-        // TODO implement the resolver
-        //print_r($args);
-        
-        return response(
-            [
-                'message' => 'Medical Record Created Successfully',
-                'status' => 200
-            ],
-            200
-        );
+        $request = $args['medical_record'];
+
+        // validate the input data
+        $validator = Validator::make($args['medical_record'], [
+            'patient_id' => 'required',
+            'ctscan_id' => 'required',
+            'mri_id' => 'required',
+            'xrays' => 'required',
+            'ultrasounds' => 'required'
+        ]);
+
+
+        $request = (object) $request;
+
+        // create new medical record
+        $medical_record = new MedicalRecord;
+        $medical_record_xray_option = new MedicalRecordXrayOption;
+        $medical_record_ultrasound_option = new MedicalRecordUltrasoundOption;
+
+        // set medical record id
+        // generate unsigned big integer unique id
+        $medical_record->medical_record_id = MedicalRecord::max('medical_record_id') + 1;
+
+        // set medical record name
+        $medical_record->patient_id = $request->patient_id;
+        $medical_record->ctscan_id = $request->ctscan_id;
+        $medical_record->mri_id = $request->mri_id;
+
+        $xraysoptions = $request->xrays;
+        $ultrasoundsoptions = $request->ultrasounds;
+
+        // batch insert xray options
+        $medical_record_xray_options = [];
+        foreach ($xraysoptions as $xraysoption) {
+            $xraysoption = (object) $xraysoption;
+            // set medical record id
+            $medical_record_xray_option->medical_record_id = $medical_record->medical_record_id;
+            // set xray id
+            $medical_record_xray_option->xray_option_name = $xraysoption->name;
+            $medical_record_xray_option->xray_option_id = $xraysoption->xray_id;
+
+            // save new medical record xray option
+            $medical_record_xray_options[] = $medical_record_xray_option->toArray();
+
+        }
+
+        // batch insert ultrasound options
+        $medical_record_ultrasound_options = [];
+        foreach ($ultrasoundsoptions as $ultrasoundsoption) {
+            $ultrasoundsoption = (object) $ultrasoundsoption;
+            // set medical record id
+            $medical_record_ultrasound_option->medical_record_id = $medical_record->medical_record_id;
+            // set ultrasound id
+            $medical_record_ultrasound_option->ultrasound_option_name = $ultrasoundsoption->name;
+            $medical_record_ultrasound_option->ultrasound_option_id = $ultrasoundsoption->ultrasound_id;
+
+            // save new medical record ultrasound option
+            $medical_record_ultrasound_options[] = $medical_record_ultrasound_option->toArray();
+            
+        }
+
+        MedicalRecordXrayOption::insert($medical_record_xray_options);
+        MedicalRecordUltrasoundOption::insert($medical_record_ultrasound_options);
+
+        $medical_record->save();
+
+        //return response with status code and message
+        return response()->json([
+            'status' => 200,
+            'message' => 'Medical record created successfully',
+            'medical_record' => $medical_record,
+            'medical_record_xray_options' => $medical_record_xray_options,
+            'medical_record_ultrasound_options' => $medical_record_ultrasound_options
+        ], 200);
     }
+
 }
